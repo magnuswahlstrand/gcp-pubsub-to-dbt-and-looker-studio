956df31382f1e0f46b3e0bef27d6606f21f61b3c
magnus <magnus.wahlstrand@gmail.com>
2024-03-24 13:08:07 +0100

    PubSub to BigQuery working

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,36 +25,28 @@ resource "google_bigquery_table" "bq_table" {
   dataset_id = google_bigquery_dataset.bq_dataset.dataset_id
   table_id   = "${local.app_name}_table"
 
-
   deletion_protection = false
 
-  schema = jsonencode([
+  schema = file("${path.module}/schemas/${local.topic_name}_output.json")
+#  schema = jsonencode([
+#    {
+#      "name" : "message",
+#      "type" : "JSON",
+#      "mode" : "NULLABLE"
+#    }
+#  ])
+}
+
+module "pubsub-dlq" {
+  source     = "terraform-google-modules/pubsub/google"
+  project_id = var.project
+
+  topic              = "${local.topic_name}_dlq"
+  pull_subscriptions = [
     {
-      "name" : "subscription_name",
-      "type" : "STRING",
-      "mode" : "REQUIRED"
-    },
-    {
-      "name" : "message_id",
-      "type" : "STRING",
-      "mode" : "REQUIRED"
-    },
-    {
-      "name" : "publish_time",
-      "type" : "TIMESTAMP",
-      "mode" : "REQUIRED"
-    },
-    {
-      "name" : "data",
-      "type" : "JSON", // Use BYTES if the data is binary
-      "mode" : "REQUIRED"
-    },
-    {
-      "name" : "attributes",
-      "type" : "JSON", // Use JSON if the attributes are in JSON format
-      "mode" : "REQUIRED"
+      name = "${local.topic_name}_dlq"
     }
-  ])
+  ]
 }
 
 module "pubsub-main" {
@@ -68,9 +60,10 @@ module "pubsub-main" {
       name = "pubsub_to_big_query_${local.topic_name}"              // required
 
       table               = "${var.project}.${google_bigquery_dataset.bq_dataset.dataset_id}.${google_bigquery_table.bq_table.table_id}"
-      use_topic_schema    = false                    // optional
-      write_metadata      = true                   // optional
+      use_topic_schema    = true                    // optional
+      write_metadata      = false                   // optional
       drop_unknown_fields = false                   // optional
+      dead_letter_topic   = module.pubsub-dlq.id // optional
     }
   ]
   schema = {
