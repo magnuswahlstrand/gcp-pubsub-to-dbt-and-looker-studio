44f5914879c5e3000bee67ee52df628d7832eae2
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2024-03-24 15:07:33 +0000

    adapt to new order model

--- dbt/models/staging/demo/stg_demo__orders.sql
+++ dbt/models/staging/demo/stg_demo__orders.sql
@@ -1,10 +1,23 @@
 with
     orders as (
         select *
-        from {{ source("order_platform_order_created", "order_platform_table") }}
+        from {{ source("order_platform", "order_created") }}
+    ),
+    order_rows as (
+        select 
+            o.order_id,
+            o.customer_id,
+            o.order_date,
+            item.item_id,
+            item.quantity,
+            CAST(item.price AS NUMERIC) as price,
+        from orders as o, unnest(o.items) as item
     ),
     final as (
-        select * from orders
+        select 
+            *,
+            price * quantity as line_revenue,
+        from order_rows
     )
 select *
 from final
