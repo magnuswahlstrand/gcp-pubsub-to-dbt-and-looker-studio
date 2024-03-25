d2a6868f4ad13585636b1d720cdb0709e5c90049
magnus <magnus.wahlstrand@gmail.com>
2024-03-25 22:42:14 +0100

    Fix model

--- dbt/models/intermediate/demo/int_order_rows_and_customers_joined.sql
+++ dbt/models/intermediate/demo/int_order_rows_and_customers_joined.sql
@@ -4,33 +4,32 @@ with
         from {{ ref("stg_demo__customers") }}
     ),
     countries as (
-        select * 
+        select *
         from {{ ref("stg_countries") }}
     ),
     customers_with_country as (
-        select 
+        select
             c.*,
             country.name as country_name,
             country.region as country_region,
             country.sub_region as country_sub_region,
-            from customers as c
-        left join countries as country on c.country_code = country.alpha_2
+        from customers as c
+                 left join countries as country on c.country_code = country.alpha_2
     ),
-
-
     order_rows as (
-        select * 
+        select *
         from {{ ref("stg_demo__orders") }}
     ),
     rows_with_customers as (
-        select 
+        select
             o.*,
+            c.name as customer_name,
             c.country_code as customer_country_code,
             c.city as customer_city,
             c.country_name as customer_country_name,
             c.country_region as customer_country_region,
             c.country_sub_region as customer_country_sub_region,
         from order_rows as o
-        left join customers_with_country as c on o.customer_id = c.customer_id
+                 left join customers_with_country as c on o.customer_id = c.customer_id
     )
-select * from customers_with_country
+select * from rows_with_customers
